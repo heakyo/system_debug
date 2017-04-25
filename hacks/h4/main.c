69f0f61b3efdd0edf8fa77b15d4bd6dde18f5338
Marvin <heakyo163@163.com>
2017-04-25 20:27:53 +0800

    added main.c for hacks4
    Signed-off-by: Marvin <heakyo163@163.com>

---
+++ hacks/h4/main.c
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2017年04月25日 19时59分24秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int *a = NULL;
+	*a = 1;
+
+	return 0;
+}
